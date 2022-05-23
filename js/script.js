ab667c528aef0b9daf2d1ad72e85adee6274015f
jen-lee27 <jlee689@myseneca.ca>
2022-05-23 03:09:24 -0400

    added introduction + applied parallax scrolling effects

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
 // upon page load
 addEventListener("DOMContentLoaded", function () {
-    let header = document.getElementById("nav");
-    let headerOffset = header.offsetTop;
+    //header text animation
+    headerTokenize();
+    document.getElementById("header").querySelectorAll(".split span").forEach((span, index) => {
+        setTimeout(() => {
+            span.classList.add("show");
+        }, 50 * index);
+    })
+
+    let navOffset = document.getElementById("nav").offsetTop;
+    
     window.addEventListener("scroll", function() {
         let scrollTop = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
 
-        console.log(scrollTop);
-        if(scrollTop >= headerOffset)
-            header.classList.add("on");
+        //nav
+        if(scrollTop >= navOffset)
+            nav.classList.add("on");
         else
-            header.classList.remove("on");
+            nav.classList.remove("on");
+
+        //skills section
+        document.querySelectorAll(".s-skills .card").forEach((elem, index) => {
+            if(scrollTop >= elem.offsetTop - this.window.innerHeight/2) {
+                document.querySelector(".s-skills .card:nth-child(" + (index + 1) + ")").classList.add("show");
+            }
+        });
     });
 });
+
+//letter tokenizing for all elements of header
+let headerTokenize = function() {
+    document.querySelectorAll("header .split").forEach(elem => {
+        let text = elem.innerText;
+        let split = text.split('').join("</span><span aria-hidden='true'>");
+        split = "<span aria-hidden='true'>" + split + "</span>";
+        //** aria-hidden to take web standardization into account(screen readers)
+        elem.innerHTML = split;
+        elem.setAttribute("aria-label", text);
+    });    
+};
